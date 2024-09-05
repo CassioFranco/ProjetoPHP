373761ca9891769a271c19d12d49ed7ac21e1b92
CassioFranco <engs-cassiofranco@camporeal.edu.br>
2024-09-05 16:44:28 -0300

    estoque

--- projeto.php
+++ projeto.php
@@ -83,3 +83,69 @@ function login(&$usuarios) {
         return false;
     }
 }
+
+// Estoque e configurações
+function gerenciar_estoque(&$produtos) {
+    while (true) {
+        limpar_tela();
+        echo "\n----- Gerenciamento de Estoque -----\n";
+        echo "1. Adicionar Produto\n";
+        echo "2. Editar Produto\n";
+        echo "3. Excluir Produto\n";
+        echo "4. Visualizar Produtos\n";
+        echo "5. Voltar\n";
+        $opcao = ler_entrada("Escolha uma opção: ");
+
+        switch ($opcao) {
+            case '1':
+                $nome = ler_entrada("Nome do Produto: ");
+                $valor = ler_entrada("Valor do Produto: ");
+                $produtos[] = ['id' => count($produtos) + 1, 'nome' => $nome, 'valor' => $valor];
+                echo "Produto adicionado com sucesso!\n";
+                break;
+
+            case '2':
+                
+                $id = ler_entrada("ID do Produto a Editar: ");
+
+                
+
+                foreach ($produtos as &$produto) {
+                    
+                    if ($produto['id'] == $id) {
+                        $produto['nome'] = ler_entrada("Novo Nome do Produto: ");
+                        $produto['valor'] = ler_entrada("Novo Valor do Produto: ");
+                        echo "Produto editado com sucesso!\n";
+                        break;
+                    }
+                }
+                break;
+
+            case '3':
+                $id = ler_entrada("ID do Produto a Excluir: ");
+                foreach ($produtos as $index => $produto) {
+                    if ($produto['id'] == $id) {
+                        unset($produtos[$index]);
+                        echo "Produto excluído com sucesso!\n";
+                        break;
+                    }
+                }
+                $produtos = array_values($produtos); 
+                break;
+
+            case '4':
+                echo "\n----- Produtos em Estoque -----\n";
+                foreach ($produtos as $produto) {
+                    echo "ID: " . $produto['id'] . " | Nome: " . $produto['nome'] . " | Valor: R$" . $produto['valor'] . "\n";
+                }
+                ler_entrada("\nPressione Enter para continuar...");
+                break;
+
+            case '5':
+                return;
+
+            default:
+                echo "Opção inválida. Tente novamente.\n";
+        }
+    }
+}
