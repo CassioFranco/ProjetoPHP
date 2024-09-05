692aa26ea51bc20eb8858b7157036693d4201030
CassioFranco <engs-cassiofranco@camporeal.edu.br>
2024-09-05 15:11:02 -0300

    menu e ler entradas

--- projeto.php
+++ projeto.php
@@ -7,3 +7,21 @@ $usuarios = [
 
 $produtos = [];
 $orcamentos = [];
+
+
+// ler entradas
+function ler_entrada($prompt) {
+    echo $prompt;
+    return trim(fgets(STDIN));
+}
+
+// Menu
+function mostrar_menu() {
+    limpar_tela();
+    echo "\n----- Sistema de Orçamentos -----\n";
+    echo "1. Orçamentos\n";
+    echo "2. Estoque\n";
+    echo "3. Histórico de Orçamentos\n";
+    echo "4. Sair\n";
+    echo "Escolha uma opção: ";
+}
